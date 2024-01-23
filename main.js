e6bea96527b8fdab2ebf1dfa298495aa39062863
sanabelalrub2 <146954619+sanabelalrub2@users.noreply.github.com>
2024-01-23 18:51:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+ 
+var myDiv = document.getElementById("myDiv");
+myDiv.classList.add("demo");  
+
+var myHeading = document.getElementById("myHeading");
+myHeading.classList.add("vip"); 
+
+
+/*
+setTimeout(function() {
+    myHeading.classList.remove("vip");
+}, 3000);
+*/
+
+
+var myh = document.getElementById("myHeading");
+myDiv.classList.remove("vs");  
